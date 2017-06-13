a5cba7ef928650c68c954e4e0df681f350c3448f
BogojevicR <ralebogojevic@gmail.com>
2017-06-14 01:54:48 +0200

    Dodat modeli

---
+++ src/models/Agent.java
@@ -0,0 +1,29 @@
+package models;
+
+
+
+public class Agent {
+private AID id;
+	
+	public Agent() {
+		
+	}
+
+	public Agent(AID id) {
+		super();
+		this.id = id;
+	}
+
+	public AID getId() {
+		return id;
+	}
+
+	public void setId(AID id) {
+		this.id = id;
+	}
+	
+	public void handleMessage(ACLMessage message) {
+		
+	}
+ 
+}
